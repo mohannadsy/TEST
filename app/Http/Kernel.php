8762812586902444e607b811171310258ae8b6de
Clauda <klodaalrakkad@gmail.com>
2022-04-26 14:19:03 +0300

    installing packages

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,11 +38,9 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
-        'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        'throttle:api',
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
     ];
 
     /**
