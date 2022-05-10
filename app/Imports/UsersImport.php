f48e9a2e3f1b9f4f904b60b7d84028b5eb722c6e
Clauda <klodaalrakkad@gmail.com>
2022-05-10 13:00:04 +0300

    upload

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -3,19 +3,34 @@
 namespace App\Imports;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 
-class UsersImport implements ToModel
+
+//HeadingRowFormatter::default('none');
+
+
+class UsersImport implements ToModel ,WithHeadingRow
 {
+
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return User|null
+     */
     public function model(array $row)
     {
         return new User([
-            //
+            'name'  => $row['name'],
+            'email' => $row['email'],
+
+            'password' => Hash::make($row['password']),
         ]);
     }
+//    public function headingRow()
+//    {
+//        return 2;
+//    }
 }
