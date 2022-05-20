77d81fddf2fa36a1e26c9c1cabd02e753705ff9b
mohannadsy <mohannadsy96@yahoo.com>
2022-05-20 10:17:41 +0300

    print

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,18 +5,16 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import print from 'vue3-print-nb'
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .use(print)
             .mixin({ methods: { route } })
             .mount(el);
     },
 });
 
 InertiaProgress.init({ color: '#4B5563' });
-app.use(print)
