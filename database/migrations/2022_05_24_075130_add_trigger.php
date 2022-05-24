dbcce387d8d42f0da3908afb39d37e3c3dbc481a
Clauda <klodaalrakkad@gmail.com>
2022-05-24 13:57:12 +0300

    database-triggers

--- database/migrations/2022_05_24_075130_add_trigger.php
+++ database/migrations/2022_05_24_075130_add_trigger.php
@@ -11,15 +11,17 @@ class AddTrigger extends Migration
     {
         DB::unprepared('CREATE TRIGGER add_User_name AFTER INSERT ON `users` FOR EACH ROW
                 BEGIN
-
-                    INSERT INTO `posts`(`id`,`title` ,`body`,`user_id`) VALUES (2,\'RABI\', \'CHANDRA\'," SELECT `LAST_INSERT_ID()` from  `users`");
+                    INSERT INTO `posts`(`id`,`title` ,`body`,`user_id`) VALUES (1,\'RABI\', \'CHANDRA\',3);
                 END');
     }
 //                     SET user_id =  "  SELECT user_id FROM users ";
 //SET id = ` SELECT LAST_INSERT_ID() from `users` `  ;
 //                   INSERT INTO `users` (`name`) VALUES (`clauda`);
-
 //INSERT INTO  `posts`(`id`,`title` ,`body`) VALUES (2,null,null);
+//SET id = ` SELECT user_id from `users`  where users.user_id =post.user_id `  ;
+//
+
+
     public function down()
     {
 //        DB::unprepared('DROP TRIGGER `add_User_name`');
